04b03af71af0dfef21a9261032f7d67ee678aec2
unknown <kstanisz@ualberta.ca>
2019-01-23 22:15:04 -0700

    old version

--- superConductor.py
+++ superConductor.py
@@ -1,48 +1,41 @@
-from matplotlib import pyplot as plt  # for plotting
+from matplotlib import pyplot as plt
 from matplotlib import animation
-import numpy as np  # for some mathematical functions
-import random  # for random number generator
+import numpy as np
+import random
 
-plt.close('all')  # close previous figure windows
+# Initialize Variables
+plt.close('all')
 
 Lx = 32
 Ly = 32
 N = Lx*Ly
 
 kT = 0.09
-NSteps = 10000  # Number of Monte Carlo Steps
-random.seed(23)  # Ofir found a cool pattern for kT=0.1, L=16
-# random.seed(1345) #cool
+NSteps = 10000
+random.seed(23)
 
 
 def getEnergy():
-    # Loop over the n.n. pairs to calculate the energy:
     cosineSum = 0
     for i in range(N):
         nn_i_right = nn_array[i][0]
         cos1 = np.cos(theta_array[i] - theta_array[nn_i_right])
-
         nn_i_up = nn_array[i][1]
         cos2 = np.cos(theta_array[i] - theta_array[nn_i_up])
-
         cosineSum = cosineSum + cos1 + cos2
-
     energyOverJ = -1*cosineSum
     return energyOverJ
 
 
 def MCStep():
-    for j in range(N):  # Do more steps for bigger lattices
+    for j in range(N):
         # Step 2:
         s = random.randint(0, N-1)  # random integer between 0 and N-1
         p_old = theta_array[s]  # store the original value of the phase
-        # p_new = 2*np.pi*random.random() #choose a random new phase
 
-        # More efficient p_new for low T:
         d_theta = 1.0*(random.random() - 0.5)
         p_new = p_old + d_theta
 
-        # Faster way to calculate E_diff:
         E_diff = 0
         # Loop over the 4 neighbours:
         for j in range(4):
@@ -56,7 +49,18 @@ def MCStep():
             theta_array[s] = p_new
 
 
-# Loop to calculate x and y for each site number i:
+# function that the animator will call NStep times:
+def animate(i):
+    # if i % 10 == 0:
+        # print("Step %d".format(i))
+    MCStep()
+
+    u_array = np.cos(theta_array)     # length along u for each rotor
+    v_array = np.sin(theta_array)     # length along v for each rotor
+    qu.set_UVC(u_array, v_array)
+
+
+# Main Running Code
 x_array = np.zeros(N)
 y_array = np.zeros(N)
 for i in range(N):
@@ -99,17 +103,6 @@ for i in range(N):
         nn_array[i][3] = i-Lx+N
 
 
-# function that the animator will call NStep times:
-def animate(i):
-    # if i % 10 == 0:
-        # print("Step %d".format(i))
-    MCStep()
-
-    u_array = np.cos(theta_array)     # length along u for each rotor
-    v_array = np.sin(theta_array)     # length along v for each rotor
-    qu.set_UVC(u_array, v_array)
-
-
 u_array = np.cos(theta_array)     # length along u for each rotor
 v_array = np.sin(theta_array)     # length along v for each rotor
 
@@ -123,7 +116,6 @@ plt.xlim(-1, Lx)
 plt.ylim(-1, Ly)
 
 # Call the animator function many times (i.e. do many MC steps):
-anim = animation.FuncAnimation(fig, animate, frames=NSteps, interval=500,
-                               repeat=False)
+anim = animation.FuncAnimation(fig, animate, frames=NSteps, interval=500, repeat=False)
 
 plt.show()
